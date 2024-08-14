8d2fcb6260c2bf72276aac2e360858e681a9a775
alvimdev <alvim103@gmail.com>
2024-08-14 08:10:56 -0300

    feat: refatorando codigos de revisao

--- lab/RevisaoAed/src/App.java
+++ lab/RevisaoAed/src/App.java
@@ -3,31 +3,57 @@ package RevisaoAed.src;
 import java.util.Scanner;
 
 public class App {
+  public static void menu() {
+    System.out.println("-------------- MENU --------------");
+    System.out.println("1. Fibonacci usando loop");
+    System.out.println("2. Fibonacci usando recursão");
+    System.out.println("3. Calcular seu IMC");
+    System.out.println("4. Sair");
+    System.out.print("Opção: ");
+  }
+
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
+    Integer choice = 0;
+    int N;
 
-    try {
-      System.out.print("Digite um numero para calcular seu FIbonacci: ");
-      int N = reader.nextInt();
+    do {
+      try {
+        menu();
+        choice = reader.nextInt();
 
-      System.out.println("Fibonacci usando loop: " + RevisaoAed.fibLoop(N));
+        switch (choice) {
+          case 1:
+            System.out.print("Digite um numero para calcular seu Fibonacci usando loop: ");
+            N = reader.nextInt();
+            System.out.println("Resultado: " + RevisaoAed.fibLoop(N));
+            break;
+          case 2:
+            System.out.print("Digite um numero para calcular seu Fibonacci usando recursão: ");
+            N = reader.nextInt();
+            System.out.println("Resultado: " + RevisaoAed.fibRec(N, 0, 1, 0, 0));
+            break;
+          case 3:
+            System.out.print("Insira seu peso: ");
+            float peso = reader.nextFloat();
 
-      System.out.println("Fibonacci usando recursão: " + RevisaoAed.fibRec(N, 0, 1, 0, 0));
+            System.out.print("Insira sua altura: ");
+            float altura = reader.nextFloat();
 
-      System.out.print('\n');
+            Imc imc = new Imc(peso, altura);
 
-      System.out.print("Insira seu peso: ");
-      float peso = reader.nextFloat();
-
-      System.out.print("Insira sua altura: ");
-      float altura = reader.nextFloat();
-
-      Imc imc = new Imc(peso, altura);
-
-      System.out.println(String.format("Seu IMC é: %.2f", imc.getImc()));
-    } catch (IllegalArgumentException e) {
-      System.err.println(e.getMessage());
-    }
+            System.out.println(String.format("Seu IMC é: %.2f", imc.getImc()));
+            break;
+          case 4:
+            System.out.println("Goodbye :)");
+            break;
+          default:
+            break;
+        }
+      } catch (IllegalArgumentException e) {
+        System.err.println(e.getMessage());
+      }
+    } while (choice != 4);
 
     reader.close();
   }
--- lab/RevisaoAed/src/RevisaoAed.java
+++ lab/RevisaoAed/src/RevisaoAed.java
@@ -3,14 +3,18 @@ package RevisaoAed.src;
 public class RevisaoAed {
   public static int fibLoop(int N) {
 
+    if (N < 0) {
+      throw new IllegalArgumentException("Não são permitidos números negativos");
+    }
+
+    if (N == 0) {
+      return 0;
+    }
+
     /* De acordo com o GJS Guide, cada variável deve ser declarada em sua respectiva linha */
     int a = 0;
     int b = 1;
     int c = a + b;
-
-    if (N <= 0) {
-      return 0;
-    }
 
     /* Em fors, as variaveis podem ser declaradas em sequencia na inicialização, caso necessário */
     for (int i = 0; i < N; i++) {
@@ -23,6 +27,10 @@ public class RevisaoAed {
   }
 
   public static int fibRec(int N, int a, int b, int c, int index) {
+    if (N < 0) {
+      throw new IllegalArgumentException("Não são permitidos números negativos");
+    }
+
     if (N == index) {
       return c;
     }
