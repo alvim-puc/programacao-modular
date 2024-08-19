ba917591259b468f02d16d9c93738631b50a1708
alvimdev <alvim103@gmail.com>
2024-08-19 10:52:03 -0300

    feat: adicionando try-catch no cadastro

--- lab/CadPessoa/src/main/App.java
+++ lab/CadPessoa/src/main/App.java
@@ -11,13 +11,13 @@ public class App {
   public static int menu(Scanner reader) {
     int choice;
 
-    System.out.println("-------------- MENU --------------");
+    System.out.println("---------------- MENU -----------------");
     System.out.println("1. Cadastrar uma pessoa");
     System.out.println("2. Listar pessoas");
     System.out.println("0. Sair");
     System.out.print("Opção: ");
     choice = reader.nextInt();
-    System.out.println("----------------------------------");
+    System.out.println("---------------------------------------");
 
     return choice;
   }
@@ -36,82 +36,90 @@ public class App {
 
       switch (choice) {
         case 1:
-          int choiceEnums;
+          try {
+            int choiceEnums;
 
-          System.out.print("Digite seu nome: ");
-          String nome = reader.nextLine();
+            System.out.print("Digite seu nome: ");
+            String nome = reader.nextLine();
 
-          System.out.print("Insira sua altura: ");
-          float altura = reader.nextFloat();
+            System.out.print("Insira sua altura: ");
+            float altura = reader.nextFloat();
 
-          System.out.print("Insira seu peso: ");
-          float peso = reader.nextFloat();
+            System.out.print("Insira seu peso: ");
+            float peso = reader.nextFloat();
 
-          reader.nextLine();
+            reader.nextLine();
 
-          System.out.print("Insira sua data de nascimento no formato AAAA-MM-DD: ");
-          String dataNascimento = reader.nextLine();
-          LocalDate data = LocalDate.parse(dataNascimento);
+            System.out.print("Insira sua data de nascimento no formato AAAA-MM-DD: ");
+            String dataNascimento = reader.nextLine();
+            LocalDate data = LocalDate.parse(dataNascimento);
 
-          System.out.println("Qual o seu estado civil? ");
-          System.out.println("1. Solteiro");
-          System.out.println("2. Casado");
-          System.out.println("3. União estável");
-          System.out.println("4. Divorciado");
-          System.out.println("5. Separado");
-          System.out.println("6. Viúvo");
-          System.out.print("Opção: ");
-          choiceEnums = reader.nextInt();
-          EstadoCivil estadoCivil = EstadoCivil.values()[--choiceEnums];
+            System.out.println("Qual o seu estado civil? ");
+            System.out.println("1. Solteiro");
+            System.out.println("2. Casado");
+            System.out.println("3. União estável");
+            System.out.println("4. Divorciado");
+            System.out.println("5. Separado");
+            System.out.println("6. Viúvo");
+            System.out.print("Opção: ");
+            choiceEnums = reader.nextInt();
+            choiceEnums = choiceEnums <= 1 || choiceEnums >= 5 ? 1 : choiceEnums;
+            EstadoCivil estadoCivil = EstadoCivil.values()[--choiceEnums];
 
-          System.out.println("Qual a sua formação acadêmica? ");
-          System.out.println("1. Nenhuma");
-          System.out.println("2. Básica");
-          System.out.println("3. Média");
-          System.out.println("4. Superior");
-          System.out.print("Opção: ");
-          choiceEnums = reader.nextInt();
-          FormacaoAcademica formacaoAcademica = FormacaoAcademica.values()[--choiceEnums];
+            System.out.println("Qual a sua formação acadêmica? ");
+            System.out.println("1. Nenhuma");
+            System.out.println("2. Básica");
+            System.out.println("3. Média");
+            System.out.println("4. Superior");
+            System.out.print("Opção: ");
+            choiceEnums = reader.nextInt();
+            choiceEnums = choiceEnums <= 1 || choiceEnums >= 5 ? 1 : choiceEnums;
+            FormacaoAcademica formacaoAcademica = FormacaoAcademica.values()[--choiceEnums];
 
-          System.out.println("Qual a sua profissão? ");
-          System.out.println("1. Desempregado");
-          System.out.println("2. Estudante");
-          System.out.println("3. Autônomo");
-          System.out.println("4. CLT");
-          System.out.println("5. Empresário");
-          System.out.print("Opção: ");
-          choiceEnums = reader.nextInt();
-          Profissao profissao = Profissao.values()[--choiceEnums];
+            System.out.println("Qual a sua profissão? ");
+            System.out.println("1. Desempregado");
+            System.out.println("2. Estudante");
+            System.out.println("3. Autônomo");
+            System.out.println("4. CLT");
+            System.out.println("5. Empresário");
+            System.out.print("Opção: ");
+            choiceEnums = reader.nextInt();
+            choiceEnums = choiceEnums <= 1 || choiceEnums >= 5 ? 1 : choiceEnums;
+            Profissao profissao = Profissao.values()[--choiceEnums];
 
-          System.out.println("Você tem vida social? S ou N");
-          boolean vidaSocial = reader.nextBoolean();
+            System.out.println("Você tem vida social? 'true' ou 'false'");
+            boolean vidaSocial = reader.nextBoolean();
 
-          System.out.println("Você tem algum hobby? S ou N");
-          boolean hobby = reader.nextBoolean();
+            System.out.println("Você tem algum hobby? 'true' ou 'false'");
+            boolean hobby = reader.nextBoolean();
 
-          System.out.println("Quantos dias você faz atividade física por semana? 0 a 7");
-          int atividadeFisica = reader.nextInt();
+            System.out.println("Quantos dias você faz atividade física por semana? 0 a 7");
+            int atividadeFisica = reader.nextInt();
 
-          System.out.println("Qual a sua saúde? 1 a 10");
-          int saude = reader.nextInt();
+            System.out.println("Qual a sua saúde? 1 a 10");
+            int saude = reader.nextInt();
 
-          Pessoa pessoa =
-              new Pessoa(
-                  nome,
-                  altura,
-                  peso,
-                  data,
-                  estadoCivil,
-                  formacaoAcademica,
-                  profissao,
-                  vidaSocial,
-                  hobby,
-                  atividadeFisica,
-                  saude);
+            Pessoa pessoa =
+                new Pessoa(
+                    nome,
+                    altura,
+                    peso,
+                    data,
+                    estadoCivil,
+                    formacaoAcademica,
+                    profissao,
+                    vidaSocial,
+                    hobby,
+                    atividadeFisica,
+                    saude);
 
-          pessoas.add(pessoa);
+            pessoas.add(pessoa);
 
-          System.out.println("Pessoa cadastrada com sucesso!");
+            System.out.println("Pessoa cadastrada com sucesso!");
+          } catch (IllegalArgumentException e) {
+            System.err.println("Erro ao cadastrar usuário: : " + e.getMessage());
+          }
+
           break;
 
         case 2:
