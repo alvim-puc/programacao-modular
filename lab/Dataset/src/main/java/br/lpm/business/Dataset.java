b50ccf618e81836efe34914a80d9de50a867a223
alvimdev <alvim103@gmail.com>
2024-08-28 22:13:33 -0300

    update

--- lab/Dataset/src/main/java/br/lpm/business/Dataset.java
+++ lab/Dataset/src/main/java/br/lpm/business/Dataset.java
@@ -1,5 +1,9 @@
 package br.lpm.business;
 
 public class Dataset {
+  private static final int MAX_PESSOAS = 55;
+  private int qtdPessoas = 0;
+  private Pessoa[] pessoas = new Pessoa[MAX_PESSOAS];
+
   
 }
